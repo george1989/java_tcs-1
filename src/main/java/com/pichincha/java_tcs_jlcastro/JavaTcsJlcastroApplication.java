c060b2ff2c2723506e11e28f4c53c3df9d770507
jlcastro <jlcastro@pichincha.com>
2022-04-01 11:31:44 -0500

    Fuentes Iniciales

---
+++ src/main/java/com/pichincha/java_tcs_jlcastro/JavaTcsJlcastroApplication.java
@@ -0,0 +1,13 @@
+package com.pichincha.java_tcs_jlcastro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTcsJlcastroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTcsJlcastroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pichincha/java_tcs_jlcastro/JavaTcsJlcastroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pichincha.java_tcs_jlcastro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTcsJlcastroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
